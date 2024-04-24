981c923b9df39ea429dfe28aa34f33ba3ce74e82
Osama Ashraf <s-osama.abdelwahhab@zewailcity.edu.eg>
2024-04-24 16:31:21 +0200

    Enahnce Code Clarity

--- codeforces.py
+++ codeforces.py
@@ -4,14 +4,35 @@ import os
 import json
 from git import Repo
 repo = Repo.init('Code\\').git
+# TODO: Support for all languages
+
+
+def clear(text: str)->str:
+    cleared_text = ""
+    for i in text:
+        if i not in SPECIAL_CHARACTERS:
+            cleared_text += i
+
+    return cleared_text
+
+def send_message(token: str, chat_id: str):
+    message = f"Problem {problem['problem_index']} - {problem['problem_name']} has been added to the repository ✅\n\nLink: https://codeforces.com/contest/{problem['contest_id']}/problem/{problem['problem_index']}"
+
+    method = f'sendMessage?chat_id={chat_id}&text={message}'
+
+    url = f'https://api.telegram.org/bot{token}/{method}'
+    r = requests.get(url)
+
+
+HANDLE = "Osama_Ashraf01"
+URL = f"https://codeforces.com/api/user.status?handle={HANDLE}"
+SPECIAL_CHARACTERS = ['/', '\\', ':', '*', '?', '"', '<', '>', '|']
+TELEGRAM_TOKEN = "6997981447:AAG9lbuT79DwoaxrvtfrLOWfY2VPmLDd3q4"
+CHAT_ID = "-1002082990507"
+
 
 # Get submissions from codeforces
-handle = "YOUR_HANDLE"
-URL = f"https://codeforces.com/api/user.status?handle={handle}"
 data = requests.get(URL, verify=False).json()['result']
-SPECIAL_CHARACTERS = ['/', '\\', ':', '*', '?', '"', '<', '>', '|']
-
-
 
 # Filter Accepted Submissions
 accepted = []
@@ -29,12 +50,6 @@ with open('data.json', 'w') as f:
     json.dump(accepted, f)
 
 
-
-
-
-
-
-
 # Scraping the codeforces website for the code of the problems
 with open('data.json', 'rb') as f:
     data = json.load(f)
@@ -47,40 +62,33 @@ with open('uploaded.json', 'rb') as f:
 codes = []
 
 for problem in data[::-1]:
-    
-        URL = f"https://codeforces.com/contest/{problem['contest_id']}/submission/{problem['submission_id']}"
-        r = requests.get(URL, verify=False)
-
-        soup = BeautifulSoup(r.content, 'html5lib')
-
-        code = soup.select("pre")
-        if not os.path.exists('Code\\'+problem['problem_index']):
-            os.makedirs('Code\\'+problem['problem_index'])
-
-        cleared_name = ''
-        for i in problem['problem_name']:
-            if i not in SPECIAL_CHARACTERS:
-                cleared_name += i
-
-        with open(f'Code\\{problem['problem_index']}\\{problem['problem_index']+' - '+cleared_name}.cpp', 'w') as f:
-            f.write(f"// Link To Problem: https://codeforces.com/contest/{problem['contest_id']}/problem/{problem['problem_index']}\n\n")
-            f.write(code[0].text)
+    URL = f"https://codeforces.com/contest/{problem['contest_id']}/submission/{problem['submission_id']}"
+    PROBLEM_LINK = f"https://codeforces.com/contest/{problem['contest_id']}/problem/{problem['problem_index']}"
+    cleared_name = clear(problem['problem_name'])
+    FILE_NAME = f'Code\\{problem['problem_index']}\\{problem['problem_index']+' - '+cleared_name}.cpp'
+    INDEX = f"Code\\{problem['problem_index']}"
         
+    if not os.path.exists(INDEX):   # New index
+        os.makedirs(INDEX)
+
+    # Get Code
+    r = requests.get(URL, verify=False)
+    soup = BeautifulSoup(r.content, 'html5lib')
+    code = soup.select("pre")[0]
+    with open(FILE_NAME, 'w') as f:
+        f.write(f"// Link To Problem: {PROBLEM_LINK}\n\n")
+        f.write(code.text)
+
+
+    # Check if the problem already exists
+    if problem not in uploaded:
+        # Add & Commit
         repo.add('*')
         index = Repo.init("Code\\").index
         index.commit(f"{problem['problem_tags']}")
-
-
-        if problem not in uploaded:
-            # Send Message on Telegram
-            token = 'YOUR_BOT_TOKEN'
-            chat_id = 'CHAT_ID'
-            message = f"Problem {problem['problem_index']} - {problem['problem_name']} has been added to the repository ✅\n\nLink: https://codeforces.com/contest/{problem['contest_id']}/problem/{problem['problem_index']}"
-
-            method = f'sendMessage?chat_id={chat_id}&text={message}'
-
-            URL = f'https://api.telegram.org/bot{token}/{method}'
-            r = requests.get(URL)
+        # Send Message on Telegram
+        send_message(TELEGRAM_TOKEN, CHAT_ID)
+            
 
 
 # Update the uploaded file
